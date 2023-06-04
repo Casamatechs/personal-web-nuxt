782e7211e80e4f8dc4ea3d73a12075b3b0bdb276
Carlos Sanchez <carlos@sanchez.kr>
2023-06-04 10:57:56 +0900

    Initial port from Next to Nuxt

---
+++ components/footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="flex flex-row justify-center gap-x-5">
+    <a class="h-8 w-8 flex-initial" href="mailto:carlos@sanchez.kr">
+      <img
+        alt="Email me!"
+        class="dark:invert"
+        width="32"
+        height="32"
+        src="/email.svg"
+      />
+    </a>
+    <a class="h-8 w-8 flex-initial" href="https://github.com/Casamatechs">
+      <img
+        alt="Check out my Github!"
+        class="dark:invert"
+        width="32"
+        height="32"
+        src="/github-mark.svg"
+      />
+    </a>
+    <a
+      class="h-8 w-8 flex-initial"
+      href="https://www.linkedin.com/in/csanchezmarin/"
+    >
+      <img
+        alt="Check out my LinkedIn!"
+        class="dark:invert"
+        width="32"
+        height="32"
+        src="/linkedin.svg"
+      />
+    </a>
+  </footer>
+</template>
